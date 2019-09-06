e09226d4c2ecc39f9f287c581abfcc6ff9532dc2
osunguk <osunguk@gmail.com>
2019-09-06 11:33:11 +0900

    tutorial3 JSX

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  render() {
+    const value = 1;
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '12px'
+    };
+    return (
+      <fragment>
+        <div className='조건문 테스트'>
+          {
+            (() => {
+              if (value === 1) return (<div>하나</div>);
+              if (value === 2) return (<div>둘</div>);
+              if (value === 3) return (<div>셋</div>);
+            })()
+          }
+        </div>
+        <div className='스타일 테스트' style={style}>
+          hi there
+        </div>
+        <div className='App'>
+          css파일 import 해서 스타일링
+        </div>
+        <div className='주석 테스트' //태그 속성안에 주석 넣을때
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {/* 태그 사이에는 이렇게 */}
+        </div>
+      </fragment>
+    );
+  }
 }
 
 export default App;
